889d3ecef112e57b598fce32ad6c4444967849db
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-06-22 15:40:16 +0700

    Core Data

--- TaskManagementCoreData/View/NewTask.swift
+++ TaskManagementCoreData/View/NewTask.swift
@@ -14,6 +14,10 @@ struct NewTask: View {
     @State var taskTitle: String = ""
     @State var taskDescription: String = ""
     @State var taskDate: Date = Date()
+    
+    //MARK: - Core Data Context
+    @Environment(\.managedObjectContext) var context
+    @EnvironmentObject var taskModel: TaskViewModel
     var body: some View {
         NavigationView {
             List {
@@ -29,12 +33,15 @@ struct NewTask: View {
                     Text("Task Description")
                 }
                 
-                Section {
-                    DatePicker("", selection: $taskDate)
-                        .datePickerStyle(.graphical)
-                        .labelsHidden()
-                } header: {
-                    Text("Task Date")
+                // Disabling Date for Edit
+                if taskModel.editTask == nil {
+                    Section {
+                        DatePicker("", selection: $taskDate)
+                            .datePickerStyle(.graphical)
+                            .labelsHidden()
+                    } header: {
+                        Text("Task Date")
+                    }
                 }
             }
             .listStyle(.insetGrouped)
@@ -46,7 +53,20 @@ struct NewTask: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button("Save") {
+                        if let task = taskModel.editTask {
+                            task.taskTitle = taskTitle
+                            task.taskDescription = taskDescription
+                        } else {
+                            let task = Task(context: context)
+                            task.taskTitle = taskTitle
+                            task.taskDescription = taskDescription
+                            task.taskDate = taskDate
+                        }
                         
+                        // Saving
+                        try? context.save()
+                        // Dismissing View
+                        dismiss()
                     }
                     .disabled(taskTitle == "" || taskDescription == "")
                 }
@@ -55,6 +75,13 @@ struct NewTask: View {
                     Button("Cancel") {
                         dismiss()
                     }
+                }
+            }
+            // Loading Task data if from Edit
+            .onAppear {
+                if let task = taskModel.editTask {
+                    taskTitle = task.taskTitle ?? ""
+                    taskDescription = task.taskDescription ?? ""
                 }
             }
         }
