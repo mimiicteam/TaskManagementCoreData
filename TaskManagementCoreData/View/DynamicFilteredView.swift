1080e1592071113e25899bb263c02c271cdd771e
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-06-22 15:37:04 +0700

    Updating Predicate to Filter current date Task

--- TaskManagementCoreData/View/DynamicFilteredView.swift
+++ TaskManagementCoreData/View/DynamicFilteredView.swift
@@ -15,10 +15,23 @@ struct DynamicFilteredView<Content: View, T>: View where T: NSManagedObject{
     
     //MARK: - Building Custom ForEach which will give CoreData object to build View
     init(dateToFilter: Date, @ViewBuilder content: @escaping (T) -> Content) {
+        //MARK: - Predicate to Filter current date Task
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: dateToFilter)
+        let tommorow = calendar.date(byAdding: .day, value: 1, to: today)!
+        
+        // Filter Key
+        let filterKey = "taskDate"
+        
+        // This will fetch task between today and tommorow which is 24 hours
+        let predicate = NSPredicate(format: "\(filterKey) >= %@ AND \(filterKey) < %@", argumentArray: [today, tommorow])
+        
         // Intializing Request With NSPredicate
-        _request = FetchRequest(entity: T.entity(), sortDescriptors: [], predicate: nil)
+        // Adding Sort
+        _request = FetchRequest(entity: T.entity(), sortDescriptors: [.init(keyPath: \Task.taskDate, ascending: false)], predicate: predicate)
         self.content = content
     }
+    
     
     var body: some View {
         Group {
